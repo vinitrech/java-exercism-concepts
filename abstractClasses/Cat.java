7a7066e69402a5b2c46aa6e5975084d9692e77db
Vinícius Tonello Rech <vinitrech@pop-os.localdomain>
2022-11-03 14:02:14 -0300

    exercises

---
+++ abstractClasses/Cat.java
@@ -0,0 +1,7 @@
+public class Cat extends Animal {
+    @Override
+    public void makeNoise() { // method implemented following the signature defined in the parent abstract
+                              // class
+        System.out.println("Meow");
+    }
+}
---
+++ abstractClasses/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Cat myCat = new Cat();
+
+        System.out.println(myCat);
+    }
+}

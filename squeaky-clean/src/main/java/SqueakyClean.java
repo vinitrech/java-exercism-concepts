15cb3bf809b0e98425642ee6972335b809ff6dbc
Vinícius Tonello Rech <vinitrech@pop-os.localdomain>
2022-11-03 16:10:27 -0300

    Chars

--- squeaky-clean/src/main/java/SqueakyClean.java
+++ squeaky-clean/src/main/java/SqueakyClean.java
@@ -1,5 +1,27 @@
 class SqueakyClean {
     static String clean(String identifier) {
-        throw new UnsupportedOperationException("Please implement the (static) SqueakyClean.clean() method");
+        StringBuilder result = new StringBuilder();
+        boolean makeUpper = false;
+
+        for (int i = 0; i < identifier.length(); i++) {
+
+            if (Character.isSpaceChar(identifier.charAt(i)) == true) {
+                result.append('_');
+            } else if (Character.isISOControl(identifier.charAt(i)) == true) {
+                result.append("CTRL");
+            } else if (identifier.charAt(i) == '-') {
+                makeUpper = true;
+            } else if (Character.isLetter(identifier.charAt(i)) == true
+                    && !(identifier.charAt(i) >= 'α' && identifier.charAt(i) <= 'ω')) {
+                if (makeUpper) {
+                    result.append(Character.toUpperCase(identifier.charAt(i)));
+                    makeUpper = false;
+                } else {
+                    result.append(identifier.charAt(i));
+                }
+            }
+        }
+
+        return result.toString();
     }
 }

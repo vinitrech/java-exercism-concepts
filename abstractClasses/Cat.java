ab9e94c2e7ba6f7d2fc9899d6ec65e0f769a7fd7
Vinícius Tonello Rech <vinitrech@pop-os.localdomain>
2022-11-05 11:15:18 -0300

    removing concepts that are not exercism exercises

--- abstractClasses/Cat.java
+++
@@ -1,7 +0,0 @@
-public class Cat extends Animal {
-    @Override
-    public void makeNoise() { // method implemented following the signature defined in the parent abstract
-                              // class
-        System.out.println("Meow");
-    }
-}
--- abstractClasses/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Cat myCat = new Cat();
-
-        System.out.println(myCat);
-    }
-}
